b5da702960a339d1011037902766acca28dc1019
sivarajesh-gudala <sivarajesh480@gmail.com>
2021-06-01 12:59:30 +0530

    css and html optimization

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,6 @@
 import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { MatCheckboxChange } from '@angular/material/checkbox';
-import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { DarkModeService } from 'angular-dark-mode';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
--- src/app/shared/coma-seperator.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ComaSeperatorPipe } from './coma-seperator.pipe';
-
-describe('ComaSeperatorPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ComaSeperatorPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
