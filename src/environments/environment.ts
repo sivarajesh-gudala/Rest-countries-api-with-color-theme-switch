8c26f3a299d5534c439989b92bf3b710e0636c9b
sivarajesh-gudala <sivarajesh480@gmail.com>
2021-06-21 09:04:16 +0530

    new changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   production: false,
-  initializeApp: {
-    apiKey: 'AIzaSyDADMX4uCVx2z_0rAgyYpjxbkUnfZYnyaA',
-    authDomain: 'rest-countries-98d9c.firebaseapp.com',
-    projectId: 'rest-countries-98d9c',
-    storageBucket: 'rest-countries-98d9c.appspot.com',
-    messagingSenderId: '1047031453639',
-    appId: '1:1047031453639:web:8862f2c6b14f2b957cabe2',
-  },
+  // initializeApp: {
+  //   apiKey: 'AIzaSyDADMX4uCVx2z_0rAgyYpjxbkUnfZYnyaA',
+  //   authDomain: 'rest-countries-98d9c.firebaseapp.com',
+  //   projectId: 'rest-countries-98d9c',
+  //   storageBucket: 'rest-countries-98d9c.appspot.com',
+  //   messagingSenderId: '1047031453639',
+  //   appId: '1:1047031453639:web:8862f2c6b14f2b957cabe2',
+  // },
   apiCountriesList: 'https://restcountries.eu/rest/v2/',
   geoAPI:
     'https://geo.ipify.org/api/v1?apiKey=at_Y6YoKAMjsUQEY3KHyEH7vZ8oCVvAp',
